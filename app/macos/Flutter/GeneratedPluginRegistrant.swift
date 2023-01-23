58964402383f250d26f0a9b59ba292280640b0b3
iHTCboy <ihetiancong@gmail.com>
2023-01-23 11:03:01 +0800

    fix：Support M1 Architecture settings

--- app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import audioplayers_darwin
 import flutter_local_notifications
 import path_provider_macos
-import shared_preferences_macos
+import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 
